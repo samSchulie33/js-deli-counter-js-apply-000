55655bac8676bce505051993de61fa3847932a87
Sam schulenberg <schu3673@umn.edu>
2020-01-27 21:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function nowServing(katzDeliLine){
     return 'There is nobody waiting to be served!'
   }
   else{
-    return 'currently serving '+katzDeliLine[0];
+    return 'Currently serving '+katzDeliLine[0];
     katzDeliLine.shift();
   }
 }
 function currentLine(katzDeliLine){
+  let lineOrder=[];
   if(katzDeliLine.length===0){
     return 'The line is currently empty.'
   }
   else{
-    let lineOrder=[];
     for(let x=0;x<katzDeliLine.length;x++){
       lineOrder[x]=x+1+'. '+katzDeliLine[x]+' ';
     }
